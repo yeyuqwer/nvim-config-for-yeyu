42bd0056bbd3cb34ef17d27519e3a12fd80ad240
Ye Yu <nearjilt@gmail.com>
2026-01-20 20:56:03 +0800

    feat: add mac-like ctrl-f/b/p/n keymaps for nvim

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -7,3 +7,17 @@ vim.keymap.set("n", "H", "^", { desc = "line start" })
 vim.keymap.set("n", "L", "g_", { desc = "line end" })
 vim.keymap.set("x", "H", "^", { desc = "line start" })
 vim.keymap.set("x", "L", "g_", { desc = "line end" })
+
+-- * 保留原生 mac 的上下左右移动组合键
+vim.keymap.set("i", "<C-f>", "<Right>", { noremap = true, desc = "forward char" })
+vim.keymap.set("i", "<C-b>", "<Left>",  { noremap = true, desc = "backward char" })
+
+vim.keymap.set("i", "<C-p>", "<Up>",    { noremap = true, desc = "previous line" })
+vim.keymap.set("i", "<C-n>", "<Down>",  { noremap = true, desc = "next line" })
+
+-- Command-line mode (:/ ?)
+vim.keymap.set("c", "<C-f>", "<Right>", { noremap = true })
+vim.keymap.set("c", "<C-b>", "<Left>",  { noremap = true })
+
+vim.keymap.set("c", "<C-p>", "<Up>",    { noremap = true })
+vim.keymap.set("c", "<C-n>", "<Down>",  { noremap = true })
