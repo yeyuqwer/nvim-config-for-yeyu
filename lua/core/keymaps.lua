8c454492f2751c604fb6736041377a9c7762e1e8
Ye Yu <nearjilt@gmail.com>
2026-03-02 14:47:34 +0800

    chore: update dependencies and remove redundant keymap config

--- lua/core/keymaps.lua
+++ lua/core/keymaps.lua
@@ -37,7 +37,6 @@ map("n", "<D-b>", explorer.toggle, { desc = "Explorer (startup project dir)" })
 map({ "n", "i", "t" }, "<D-0>", navigation.focus_left_navigation, { silent = true, desc = "Focus left navigation pane" })
 map({ "n", "i", "t" }, "<D-E>", navigation.focus_left_navigation, { silent = true, desc = "Focus left navigation pane" })
 map({ "n", "i", "t" }, "<D-w>", tabs.close_tab_or_all, { silent = true, desc = "Close tab" })
-map({ "n", "i", "t" }, "<D-k>", tabs.arm_close_all_tabs, { silent = true, desc = "Arm close all tabs" })
 map({ "n", "i", "t" }, "<C-Tab>", tabs.next_tab_cycle, { silent = true, desc = "Next tab (cycle)" })
 
 -- Always search files in current startup/project root, even before opening any file.
