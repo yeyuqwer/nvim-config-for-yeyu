52c9d968eb9faa38e68e4e52b03a005bc3b02ed4
Ye Yu <nearjilt@gmail.com>
2026-04-27 18:19:21 +0800

    fix: correct cursor position after using comment shortcut

--- lua/plugins/comment.lua
+++ lua/plugins/comment.lua
@@ -1,11 +1,70 @@
+local insertColumn
+
+local function moveAfterCommentMarker(ctx)
+  local utils = require("Comment.utils")
+
+  if ctx.cmode ~= utils.cmode.comment or ctx.cmotion ~= utils.cmotion.line or ctx.range.srow ~= ctx.range.erow then
+    return
+  end
+
+  local line = vim.api.nvim_get_current_line()
+  local _, column = line:find("^%s*%S+%s?")
+
+  if column then
+    insertColumn = column
+    vim.api.nvim_win_set_cursor(0, { ctx.range.srow, math.min(column, math.max(#line - 1, 0)) })
+  end
+end
+
+local function startInsertAt(row, column)
+  vim.schedule(function()
+    local line = vim.api.nvim_buf_get_lines(0, row - 1, row, false)[1] or ""
+
+    if column >= #line and #line > 0 then
+      vim.api.nvim_win_set_cursor(0, { row, #line - 1 })
+      vim.cmd("startinsert!")
+      return
+    end
+
+    vim.api.nvim_win_set_cursor(0, { row, column })
+    vim.cmd("startinsert")
+  end)
+end
+
+local function toggleLine()
+  require("Comment.api").toggle.linewise.current()
+end
+
+local function toggleLineFromInsert()
+  vim.cmd("stopinsert")
+  insertColumn = nil
+  toggleLine()
+
+  local cursor = vim.api.nvim_win_get_cursor(0)
+  startInsertAt(cursor[1], insertColumn or cursor[2])
+  insertColumn = nil
+end
+
 return {
   {
     "numToStr/Comment.nvim",
     keys = {
-      { "<D-/>", "gcc", mode = "n", remap = true, desc = "Toggle comment line" },
-      { "<D-/>", "<Esc>gccgi", mode = "i", remap = true, desc = "Toggle comment line" },
+      {
+        "<D-/>",
+        toggleLine,
+        mode = "n",
+        desc = "Toggle comment line",
+      },
+      {
+        "<D-/>",
+        toggleLineFromInsert,
+        mode = "i",
+        desc = "Toggle comment line",
+      },
       { "<D-/>", "gc", mode = "x", remap = true, desc = "Toggle comment selection" },
     },
-    opts = {},
+    opts = {
+      post_hook = moveAfterCommentMarker,
+    },
   },
 }
